de5f7ab2142bcbc59eb31b9e00bbec9ad4ba5f10
Peter Peck <karatekid4christ@gmail.com>
2019-02-02 15:19:43 -0800

    Added header

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+const Header = props => (
+  <div className="">
+    <h2>Occupation Overview</h2>
+    <p className="title">Computer Programmers in Seattle-Tacoma-Bellevue, WA</p>
+  </div>
+);
+
+export default Header;

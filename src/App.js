190a711422dcb520a6ca3acf35653b82c8a77642
Peter Peck <karatekid4christ@gmail.com>
2019-02-03 23:26:52 -0800

    adding https in axios call

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   componentDidMount(){
 
     //Calling API with GET request
-		axios.get('http://www.mocky.io/v2/5a29b5672e00004a3ca09d33').then(response => {
+		axios.get('https://www.mocky.io/v2/5a29b5672e00004a3ca09d33').then(response => {
       // console.log(response.data);
       //Setting state from the response data
       this.setState({
